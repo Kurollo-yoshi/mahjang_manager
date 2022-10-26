c4fb774e87263c7f75b8b30d703aa800c73a40aa
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-10-26 22:21:41 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -317,6 +317,7 @@ try:
             )
 
             selection_data = data["selected_rows"]
+	    st.write(selection_data)
             if st.button("更新"):
                 for i in range(len(selection_data)):
                     update_value = {
