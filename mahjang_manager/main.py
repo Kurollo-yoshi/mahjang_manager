0a6c9e189f11c9946b442b62d5f7185d8f5eaf8f
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-10-26 22:18:31 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -319,11 +319,11 @@ try:
             selection_data = data["selected_rows"]
             if st.button("更新"):
                 for i in range(len(selection_data)):
-		    update_value = {
-		        "data":[selection_data[i][name_list[0]],selection_data[i][name_list[1]],selection_data[i][name_list[2]],selection_data[i][name_list[3]]],
-			"date":selection_data[i]["Date"],
-			"key":None
-		    }
+                    update_value = {
+                        "data":[selection_data[i][name_list[0]],selection_data[i][name_list[1]],selection_data[i][name_list[2]],selection_data[i][name_list[3]]],
+                        "date":selection_data[i]["Date"],
+                        "key":None
+                    }
                     db.update(update_value, selection_data[i]["key"])
 
                 st.success("情報が更新されました")
@@ -335,3 +335,4 @@ try:
 
 except Exception as e:
     raise
+
