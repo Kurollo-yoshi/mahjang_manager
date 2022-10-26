5198653c973faf6e9dc5a713d0d462af3d65e219
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-10-26 14:58:45 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -165,7 +165,7 @@ def chart_graph_point(dataframe):
             )
         )
     fig.update_layout(
-	atuosize=True,
+	autosize=True,
         height=500,
         width=800,
         plot_bgcolor = "#202020",
