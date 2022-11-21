c5a0836b07d7f787ee0d57dd940cf5e234a3f35f
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-11-21 17:16:03 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -54,7 +54,7 @@ mode_4 = "管理"
 circle_size = 300
 # 線グラフ
 chart_height = 300
-chart_width = 400
+chart_width = 450
 
 # 詳細データのカラム
 detail_columns = [
@@ -141,6 +141,13 @@ def chart_graph(dataframe):
         height=chart_height,
         width=chart_width,
 	autosize=True,
+	legend==dict(
+		      x=0.01,          # ①：X座標
+		      y=0.99,          # ①：Y座標
+		      xanchor='left',  # ②：X座標が凡例のどの部分を表すか
+		      yanchor='top',   # ②：Y座標が凡例のどの部分を表すか
+		      orientation='h', # ③：凡例を横並びにする
+		      )
         plot_bgcolor = "#202020",
         xaxis=dict(dtick=5),
         yaxis=dict(title="順位",dtick=1,autorange='reversed'),
