03ef2695481a5c11b1854225e10f8233c0cc86bf
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-11-21 17:37:58 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -223,7 +223,7 @@ def display_deteil(detail_dataframe):
     sum_dataframe["データ項目"] = sum_dataframe.index
     sum_dataframe = sum_dataframe[["データ項目"] + name_list]
 
-    gb = GridOptionsBuilder.from_dataframe(sum_dataframe, min_column_width=10)
+    gb = GridOptionsBuilder.from_dataframe(sum_dataframe, min_column_width=15)
     gb.configure_selection(selection_mode="single", use_checkbox=True,pre_selected_rows=[0])
     gridOptions = gb.build()
     data = AgGrid(
