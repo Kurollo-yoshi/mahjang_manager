7a33d0e04b6b1efd1232f4182f38c3b938a48a58
Kurollo-yoshi <87373502+Kurollo-yoshi@users.noreply.github.com>
2022-11-21 18:02:17 +0900

    Update main.py

--- mahjang_manager/main.py
+++ mahjang_manager/main.py
@@ -224,7 +224,7 @@ def display_deteil(detail_dataframe):
     sum_dataframe = sum_dataframe[["データ項目"] + name_list]
 
     gb = GridOptionsBuilder.from_dataframe(sum_dataframe)
-    gb.configure_column("データ項目",header_name="データ項目",width=5, panda=0.5)
+    gb.configure_column("データ項目",header_name="データ項目",width=5)
     gb.configure_column("Kurollo",header_name="Kurollo",width=2)
     gb.configure_column("Tamasuke",header_name="Tamasuke",width=2)
     gb.configure_column("ルチチ",header_name="ルチチ",width=2)
@@ -239,7 +239,8 @@ def display_deteil(detail_dataframe):
         update_mode=GridUpdateMode.SELECTION_CHANGED,
         theme="dark",
         data_return_mode=DataReturnMode.AS_INPUT,
-        height= 250
+        height= 250,
+	width = 300
         # fit_columns_on_grid_load=True
     )
     if len(data["selected_rows"])>0:
